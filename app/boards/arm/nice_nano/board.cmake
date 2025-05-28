c959b078b4e8f47a528a5de8ab8ae6adf09434e2
Naomi <33375791+Henvy-Mango@users.noreply.github.com>
2025-05-28 10:03:32 +0000

    add jlink

--- app/boards/arm/nice_nano/board.cmake
+++ app/boards/arm/nice_nano/board.cmake
@@ -1,5 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
+board_runner_args(jlink "--device=nRF52840_xxAA" "--speed=4000")
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)

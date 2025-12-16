d8f19a7b759255204cb0b3db5c154d3919dd3712
Naomi <33375791+Henvy-Mango@users.noreply.github.com>
2025-12-16 03:22:53 +0000

    fix cmake

--- app/src/main.c
+++ app/src/main.c
@@ -1,36 +1,13 @@
-/*
- * Copyright (c) 2018 Savoir-Faire Linux.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-#include <stdio.h>
-#include <zephyr/device.h>
-#include <zephyr/devicetree.h>
-
-#define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
+#include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(main);
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 int main(void)
 {
-	printf("Hello, World!\n");
-	const struct device *const dev = DEVICE_DT_GET_ANY(mpr121);
-	
-	if(!device_is_ready(dev)) {
-		LOG_ERR("Device %s is not ready", dev->name);
-		return -ENODEV;
+	while (1)
+	{
+		k_sleep(K_SECONDS(1));
 	}
-
-	// if (!dev) {
-	// 	LOG_ERR("No devices with compatible nxp,mpr121 found");
-	// 	return 0;
-	// } else if (!device_is_ready(dev)) {
-	// 	LOG_ERR("LED device %s is not ready", dev->name);
-	// 	return 0;
-	// } else {
-	// 	LOG_INF("Found LED device %s", dev->name);
-	// }
-
-	LOG_INF("Testing","");
 	return 0;
 }
